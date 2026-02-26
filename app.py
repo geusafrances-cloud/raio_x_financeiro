ddeecc7f8b3efd32c770469d57748757af3c2765
geusafrances-cloud <geusafrances@gmail.com>
2026-02-26 18:19:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.title("RAIO X FINANCEIRO")
+st.title("RAIO X FINANCEIRa")
 
 empresa = st.text_input("Empresa")
 
 if st.button("Testar"):
+
     st.write("Empresa:", empresa)
